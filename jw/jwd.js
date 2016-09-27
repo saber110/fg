d7659690474c3d06731404c27b9235cac1f6118f
kgfw <freeman105@gmail.com>
2016-09-27 11:38:21 +1000

    update

--- jw/jwd.js
+++ jw/jwd.js
@@ -1,1 +1,1 @@
-var _urls='aHR0cDovL3l5Zi5kZXB0b2ZoZWFsdGgueHl6LzEvLGh0dHBzOi8venpwLmhlYWx0aHVzYS54eXovMS8saHR0cHM6Ly96emkudXNhaGVhbHRoLnh5ei8xLw==';
+var _urls='aHR0cDovL3l5ZS5kZXB0b2ZoZWFsdGgueHl6LzEvLGh0dHBzOi8venpvLmhlYWx0aHVzYS54eXovMS8saHR0cHM6Ly96emMuaGVhbHRoaW5zLnh5ei8xLw==';
