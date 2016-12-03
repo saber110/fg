e19e1757c7dcc7179f9ecba883ca03536915aaf8
kgfw <freeman105@gmail.com>
2016-12-03 13:50:32 +1100

    update

--- jw/蓝灯3翻墙.bat
+++
@@ -1,7 +0,0 @@
-%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
-cls
-@echo off
-start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" http://www.bannedbook.org/bnews/fq/?utm_source=ffLT3
-start "" "%~dp0Lantern\lantern3.exe"
-start "" "%~dp0Lantern\setting.vbs"
-exit
