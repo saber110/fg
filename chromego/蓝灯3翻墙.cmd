c9fa22c9290501c37e6ed807cd7dd096f1c5850d
kgfw <freeman105@gmail.com>
2016-12-04 18:53:01 +1100

    update

---
+++ chromego/蓝灯3翻墙.cmd
@@ -0,0 +1,10 @@
+%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
+cls
+@echo off
+start "" "%~dp0Lantern\lantern.exe"
+IF EXIST %~dp0Browser\chrome.exe (
+    start %~dp0Browser\chrome.exe --user-data-dir=%~dp0chrome-user-data http://www.bannedbook.org/bnews/fq/?utm_source=chlt3
+) ELSE (
+    start chrome.exe --user-data-dir=%~dp0chrome-user-data http://www.bannedbook.org/bnews/fq/?utm_source=chlt3
+)
+exit 
